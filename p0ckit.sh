56309e8de7f680e1f72d33f80c64f13cc5b8245d
tgrd0813 <219890069+tgrd0813@users.noreply.github.com>
2025-12-12 23:12:16 +0200

    Small changes to p0ckit.sh file

--- p0ckit.sh
+++ p0ckit.sh
@@ -3,8 +3,8 @@
 #variables
 cntr=0
 req=5
-tst_file=".ntfr01"
-md_path="modules/"
+tst_file="/home/$USER/p0ckit/.ntfr01"
+md_path="/home/$USER/p0ckit/modules/"
 index_path="${md_path}index.txt"
 
 #module/script runner
@@ -73,11 +73,41 @@ cmd_hdlr() {
 	esac
 }
 
+#func for banner/s (planning to add more)
+banner() {
+	echo "                                                                                                            
+                                                                                                            
+                         000000000                         kkkkkkkk             iiii          tttt          
+                       00:::::::::00                       k::::::k            i::::i      ttt:::t          
+                     00:::::::::::::00                     k::::::k             iiii       t:::::t          
+                    0:::::::000:::::::0                    k::::::k                        t:::::t          
+ppppp   ppppppppp   0::::::0   0::::::0    cccccccccccccccc k:::::k    kkkkkkkiiiiiiittttttt:::::ttttttt    
+p::::ppp:::::::::p  0:::::0     0:::::0  cc:::::::::::::::c k:::::k   k:::::k i:::::it:::::::::::::::::t    
+p:::::::::::::::::p 0:::::0     0:::::0 c:::::::::::::::::c k:::::k  k:::::k   i::::it:::::::::::::::::t    
+pp::::::ppppp::::::p0:::::0 000 0:::::0c:::::::cccccc:::::c k:::::k k:::::k    i::::itttttt:::::::tttttt    
+ p:::::p     p:::::p0:::::0 000 0:::::0c::::::c     ccccccc k::::::k:::::k     i::::i      t:::::t          
+ p:::::p     p:::::p0:::::0     0:::::0c:::::c              k:::::::::::k      i::::i      t:::::t          
+ p:::::p     p:::::p0:::::0     0:::::0c:::::c              k:::::::::::k      i::::i      t:::::t          
+ p:::::p    p::::::p0::::::0   0::::::0c::::::c     ccccccc k::::::k:::::k     i::::i      t:::::t    tttttt
+ p:::::ppppp:::::::p0:::::::000:::::::0c:::::::cccccc:::::ck::::::k k:::::k   i::::::i     t::::::tttt:::::t
+ p::::::::::::::::p  00:::::::::::::00  c:::::::::::::::::ck::::::k  k:::::k  i::::::i     tt::::::::::::::t
+ p::::::::::::::pp     00:::::::::00     cc:::::::::::::::ck::::::k   k:::::k i::::::i       tt:::::::::::tt
+ p::::::pppppppp         000000000         cccccccccccccccckkkkkkkk    kkkkkkkiiiiiiii         ttttttttttt  
+ p:::::p                                                                                                    
+ p:::::p                                                                                                    
+p:::::::p                                                                                                   
+p:::::::p                                                                                                   
+p:::::::p                                                                                                   
+ppppppppp"
+
+}
 
 #help menu
 help_menu() {
 	echo Help menu:
 	echo help -- This help menu
+	echo "use -- Use a module/script (modules by name | scritps by path)"
+	echo serach -- Search a module/script by name or path
 	echo exit/quit -- to quit the script
 
 }
@@ -86,7 +116,7 @@ help_menu() {
 menu() {
 	while true; do
 		echo -n "fw(${ld_md})# "
-		read -r cmd arg
+		read -e -r cmd arg
 		
 		[[ -z "$cmd" ]] && continue
 
@@ -121,6 +151,7 @@ app_chk() {
 			read -p "=> " -i "n" ans
 			if [ "$ans" = "y" ]; then
 				sudo pacman -S "$app"
+				((cntr++))
 			else
 				echo "Ok how you say"
 			fi
@@ -137,11 +168,11 @@ app_chk() {
 
 }
 
-if [ $USER != "root" ]; then
-	echo "Run as root!"
-	exit 0
-fi
+#if [ $USER != "root" ]; then
+#	echo "Run as root!"
+#	exit 0
+#fi
 
+banner
 t_first_run
 menu
-
