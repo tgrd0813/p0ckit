084ec91c6ed350b6782aaa222ee7089647f1d576
tgrd0813 <219890069+tgrd0813@users.noreply.github.com>
2025-12-14 21:42:38 +0200

    Masive sturcture changes somewhat fixed update/fix func added core dir and made p0ckit.sh source core files

---
+++ core/banner.sh
@@ -0,0 +1,28 @@
+#func for banner/s (planning to add more)
+banner() {
+	echo "
+
+                         000000000                         kkkkkkkk             iiii          tttt
+                       00:::::::::00                       k::::::k            i::::i      ttt:::t
+                     00:::::::::::::00                     k::::::k             iiii       t:::::t
+                    0:::::::000:::::::0                    k::::::k                        t:::::t
+ppppp   ppppppppp   0::::::0   0::::::0    cccccccccccccccc k:::::k    kkkkkkkiiiiiiittttttt:::::ttttttt
+p::::ppp:::::::::p  0:::::0     0:::::0  cc:::::::::::::::c k:::::k   k:::::k i:::::it:::::::::::::::::t
+p:::::::::::::::::p 0:::::0     0:::::0 c:::::::::::::::::c k:::::k  k:::::k   i::::it:::::::::::::::::t
+pp::::::ppppp::::::p0:::::0 000 0:::::0c:::::::cccccc:::::c k:::::k k:::::k    i::::itttttt:::::::tttttt
+ p:::::p     p:::::p0:::::0 000 0:::::0c::::::c     ccccccc k::::::k:::::k     i::::i      t:::::t
+ p:::::p     p:::::p0:::::0     0:::::0c:::::c              k:::::::::::k      i::::i      t:::::t
+ p:::::p     p:::::p0:::::0     0:::::0c:::::c              k:::::::::::k      i::::i      t:::::t
+ p:::::p    p::::::p0::::::0   0::::::0c::::::c     ccccccc k::::::k:::::k     i::::i      t:::::t    tttttt
+ p:::::ppppp:::::::p0:::::::000:::::::0c:::::::cccccc:::::ck::::::k k:::::k   i::::::i     t::::::tttt:::::t
+ p::::::::::::::::p  00:::::::::::::00  c:::::::::::::::::ck::::::k  k:::::k  i::::::i     tt::::::::::::::t
+ p::::::::::::::pp     00:::::::::00     cc:::::::::::::::ck::::::k   k:::::k i::::::i       tt:::::::::::tt
+ p::::::pppppppp         000000000         cccccccccccccccckkkkkkkk    kkkkkkkiiiiiiii         ttttttttttt
+ p:::::p
+ p:::::p
+p:::::::p
+p:::::::p
+p:::::::p
+ppppppppp"
+
+}
