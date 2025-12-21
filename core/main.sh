6d74b001a1316269b1ed4479ac56f6dda67af4e0
tgrd0813 <219890069+tgrd0813@users.noreply.github.com>
2025-12-21 17:52:10 +0200

    Finally fixed the fucking fix module

--- core/main.sh
+++ core/main.sh
@@ -24,8 +24,7 @@ cmd_hdlr() {
 				scrp_ld "${arg[@]}"
 			fi
 			;;
-		crtmnfst) find core -type f ! -name "manifest.txt" -exec sha256sum {} + | sort > "${mnfstf}"
-			;;
+		crtmnfst) crt_mnfst;;
 		search) src_mdl "${arg[@]}";;
 		update) fw_upd;;
 		fix) fw_fix;;
--- core/update_fix.sh
+++ core/update_fix.sh
@@ -1,23 +1,23 @@
 #func to check the hash value and if different then fix broken/corupted files
 fw_fix() {
-	local crnth="core/.crntmnfst.txt"
-	find core -type f ! -name "${crnth}"! -name "${mnfstf}" -exec sha256sum {} + | sort > ${crnth}
+	local crnth="${script_hm}/core/.crntmnfst.txt"
+	find core -type f ! -name ".crntmnfst.txt" ! -name "manifest.txt" -exec sha256sum {} + | sort > ${crnth}
 	chngs="$(diff $mnfstf $crnth)"
 	if [[ -n "$chngs" ]]; then
 		echo "There is something wrong"
 		echo "Fixing broken files"
 		echo "This will reset all core files to the latest version"
-		read -e -p "Do you want to continue(Y/n) " -i "y" ans
-		if [[ "${ans}" == ^[Nn]$ ]]; then
+		read -e -p "Do you want to continue(Y/n) " ans
+		if [[ "${ans}" == "n" ]]; then
 			echo "Ok not fixing the files"
-		else
+		else	
 			git fetch origin
 			git checkout origin/main -- core/
 		fi
 	else
 		echo "All files are good"
 	fi
-	rm -f "$crnth"
+	#rm -f "$crnth"
 }
 
 #func to update/fix the cli
@@ -26,4 +26,25 @@ fw_upd() {
 	sleep 1
 	git pull origin main
 
+}
+
+#func to create manifest so this shit actually wokrs
+crt_mnfst() {
+	if [[ -f "${mnfstf}" ]]; then
+		echo Manifest already exists
+		read -p "Are you sure you want to recreate the manifest(y/N)" -i "n" ans
+		if [[ "$ans" == "y" ]]; then
+			echo Ok creating manifest
+			find core -type f ! -name "manifest.txt" -exec sha256sum {} + | sort > "${mnfstf}"
+			sleep 1
+			echo manifest created
+		else
+			echo OK not creating manifest
+		fi
+	else
+		echo Ok creating manifest
+		find core -type f ! -name "manifest.txt" -exec sha256sum {} + | sort > "${mnfstf}"
+		sleep 1
+		echo manifest created
+	fi
 }
